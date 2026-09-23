228017fe0d0221944ed5583a186714670c7268ba
tanan <tanan@MORSEN>
2026-09-23 11:22:15 -0500

    terminado reporte y cliente

--- src/Vistas_Tienda/A_Dashboard.java
+++ src/Vistas_Tienda/A_Dashboard.java
@@ -160,8 +160,9 @@ public class A_Dashboard extends javax.swing.JFrame {
 
     private void btnClientesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClientesActionPerformed
         // TODO add your handling code here:
-        //D_Clientes p = new D_Clientes();
-        //mostrarPanel(p);
+        D_Clientesp p = new D_Clientesp();
+        mostrarPanel(p);
+        
     }//GEN-LAST:event_btnClientesActionPerformed
 
     private void btnVentasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVentasActionPerformed
@@ -192,24 +193,7 @@ public class A_Dashboard extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
-            logger.log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
 
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> new A_Dashboard().setVisible(true));
     }
 
---
+++ src/Vistas_Tienda/conexionLiv.java
@@ -0,0 +1,34 @@
+
+package Vistas_Tienda;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class conexionLiv {
+    
+    private static final String url = "jdbc:mysql://localhost:3307/alanastore";
+    private static final String user = "root";
+    private static final String pass = "";
+    
+    public static Connection conectar() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url,user, pass);
+
+            System.out.println("Conexion exitosa a ALANA STORE");
+
+        } catch (Exception e) {
+
+            System.out.println(
+                    "Error de conexion: " + e.getMessage()
+            );
+        }
+
+        return con;
+    }    
+    public static void main(String[] args) {
+      conectar();
+    }
+    
+}
+
